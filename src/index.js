df44d7b35df84c27e8444d0e9afd4322a378428d
Andrew Davis <andrew@gigantic.computer>
2016-11-22 16:36:38 -0600

    wip add redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { Provider } from 'react-redux'
+
+import reducers from './reducers'
+
+let store = createStore(reducers, applyMiddleware(thunk, logger()))
+
 import App from './App';
 import './index.css';
 
@@ -9,6 +18,8 @@ import makeRoutes from './views'
 const routes = makeRoutes()
 
 ReactDOM.render(
-    <App history={browserHistory} routes={routes}/>,
+  <Provider store={store}>
+    <App history={browserHistory} routes={routes}/>
+  </Provider>,
   document.getElementById('root')
 );
