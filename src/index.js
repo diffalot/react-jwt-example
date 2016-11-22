b55a396e53cb6c29915dc1b74798cacbfcbb8d10
Andrew Davis <andrew@gigantic.computer>
2016-11-22 16:46:14 -0600

    add in redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger'
+import { Provider } from 'react-redux'
+
+import reducers from './reducers'
+const logger = createLogger()
+
+let store = createStore(reducers, applyMiddleware(thunk, logger))
+
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
