97ad492e582b8f3de7eb79db7996e678cabf209f
Andrew Davis <andrew@gigantic.computer>
2016-11-22 19:00:51 -0600

    install redux-auth-wrapper

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { routerReducer, syncHistoryWithStore, routerActions, routerMiddleware } from 'react-router-redux'
+import { UserAuthWrapper } from 'redux-auth-wrapper'
+
 import './index.css';
 
+import * as reducers from './reducers'
+import { App, Home, Foo, Login } from './components'
+
+const baseHistory = browserHistory
+const routingMiddleware = routerMiddleware(baseHistory)
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routerReducer
+}))
+
+const store = createStore(reducer, applyMiddleware(routingMiddleware))
+
+const history = syncHistoryWithStore(baseHistory, store)
+
+const UserIsAuthenticated = UserAuthWrapper({
+  authSelector: state => state.user,
+  redirectAction: routerActions.replace,
+  wrapperDisplayName: 'UserIsAuthenticated'
+})
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <div>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home}/>
+          <Route path="login" component={Login}/>
+          <Route path="foo" component={UserIsAuthenticated(Foo)}/>
+        </Route>
+      </Router>
+    </div>
+  </Provider>,
   document.getElementById('root')
-);
+)
