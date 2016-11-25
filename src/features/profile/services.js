c57a290e723cb0c139b64d2e6da24118a76f2142
Andrew Davis <andrew@gigantic.computer>
2016-11-24 22:17:36 -0600

    add in service file for profile feature

---
+++ src/features/profile/services.js
@@ -0,0 +1,24 @@
+const API_URL = process.env.REACT_APP_API_URL
+
+// TODO: find out if this block is really needed
+// If we're testing, use a local storage polyfill
+// If not, use the browser one
+let localStorage
+if (process.env.NODE_ENV === 'test') {
+  localStorage = require('localStorage')
+} else {
+  localStorage = global.window.localStorage
+}
+
+export function fetchProfile () {
+  return fetch(`${API_URL}/me`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.token}`
+    }
+  })
+    .then(function (response) {
+      return response.json()
+    })
+}
