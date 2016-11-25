dfd63806d5a6b609f8a985ea620f1348026a2fe5
Andrew Davis <andrew@gigantic.computer>
2016-11-25 13:21:35 -0600

    use services.js to call api

--- src/features/profile/sagas.js
+++ src/features/profile/sagas.js
@@ -1,6 +1,5 @@
 import { takeLatest } from 'redux-saga'
 import { put } from 'redux-saga/effects'
-import fetch from 'isomorphic-fetch'
 
 import {
   FETCH_PROFILE,
@@ -10,33 +9,14 @@ import {
   // TODO: REQUEST_JWT_TOKEN
 } from './constants'
 
-const API_URL = process.env.REACT_APP_API_URL
-
-// TODO: find out if this block is really needed
-// If we're testing, use a local storage polyfill
-// If not, use the browser one
-let localStorage
-if (process.env.NODE_ENV === 'test') {
-  localStorage = require('localStorage')
-} else {
-  localStorage = global.window.localStorage
-}
+import { fetchProfile as getProfile } from './services'
 
 // Worker Sagas
 
-function * fetchProfile () {
+export function * fetchProfile () {
   yield put({type: FETCH_PROFILE_BEGIN})
   try {
-    const profile = yield fetch(`${API_URL}/me`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.token}`
-      }
-    })
-      .then(function (response) {
-        return response.json()
-      })
+    const profile = yield getProfile()
     yield put({type: FETCH_PROFILE_END, payload: profile})
   } catch (error) {
     put({type: FETCH_PROFILE_ERROR, payload: error})
@@ -45,7 +25,7 @@ function * fetchProfile () {
 
 // Watcher Sagas
 
-function * fetchProfileWatcher () {
+export function * fetchProfileWatcher () {
   yield * takeLatest(FETCH_PROFILE, fetchProfile)
 }
 
