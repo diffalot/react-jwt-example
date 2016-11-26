ddf186dead10a62886820b56718c75d7feff2cf9
Andrew Davis <andrew@gigantic.computer>
2016-11-25 19:27:19 -0600

    add FIXME

--- src/containers/Nav.test.js
+++ src/containers/Nav.test.js
@@ -4,5 +4,6 @@ import { shallow } from 'enzyme'
 import { Nav } from './Nav'
 
 it('renders without crashing', () => {
+  // FIXME: Do I need a reducer in a test for a container?
   shallow(<Nav loggedIn currentlySending={false} dispatch={jest.fn()} />)
 })
